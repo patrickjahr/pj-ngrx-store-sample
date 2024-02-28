b4dc0ff7ec6bd0d72ef9218927958bdc7362013e
patrickjahr <patrick@qupaya.com>
2024-02-28 06:40:04 +0100

    add frontend speakers feature

---
+++ libs/lets-talk/speakers/feature/src/index.ts
@@ -0,0 +1,1 @@
+export * from './lib/feature-speakers/feature-speakers.component';
---
+++ libs/lets-talk/speakers/feature/src/lib/feature-speakers/feature-speakers.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'paddsen-next-feature-speakers',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './feature-speakers.component.html',
+  styleUrl: './feature-speakers.component.scss',
+})
+export class FeatureSpeakersComponent {}
