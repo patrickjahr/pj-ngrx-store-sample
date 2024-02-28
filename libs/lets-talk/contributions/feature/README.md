f49d9cd1535506b95f7615372495bdfddce2359c
patrickjahr <patrick@qupaya.com>
2024-02-28 06:41:22 +0100

    add frontend contributions feature

---
+++ libs/lets-talk/contributions/feature/README.md
@@ -0,0 +1,7 @@
+# feature-contributions
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test feature-contributions` to execute the unit tests.
---
+++ libs/lets-talk/contributions/feature/src/lib/feature-contributions/feature-contributions.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'paddsen-next-feature-contributions',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './feature-contributions.component.html',
+  styleUrl: './feature-contributions.component.scss',
+})
+export class FeatureContributionsComponent {}
